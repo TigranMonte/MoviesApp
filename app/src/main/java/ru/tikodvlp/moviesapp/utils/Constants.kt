abffcd032e51cd8a9645bf207609f9af10ec643d
Tigran <tigranma@gmail.com>
2022-07-28 17:36:33 +0300

    Lesson-1

---
+++ app/src/main/java/ru/tikodvlp/moviesapp/utils/Constants.kt
@@ -0,0 +1,11 @@
+package ru.tikodvlp.moviesapp.utils
+
+class Constants {
+
+    object Screens {
+        const val SPLASH_SCREEN = "splash_screen"
+        const val MAIN_SCREEN = "main_screen"
+        const val DETAILS_SCREEN = "details_screen"
+
+    }
+}
